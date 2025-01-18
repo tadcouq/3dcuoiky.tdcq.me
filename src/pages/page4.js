991d71ff255ef2152df3c35c40d01fd4e27f258b
trinhgiahung2710 <trinhgiahung2710@gmail.com>
2025-01-19 01:21:55 +0700

    190125

--- src/pages/page4.js
+++ src/pages/page4.js
@@ -17,9 +17,9 @@ const controls = new OrbitControls( camera, renderer.domElement );
 camera.position.set( -10, 5, -10 );
 controls.update();
 
-const light = new THREE.AmbientLight( 0x404040, -0.01 ); // soft white light
+const light = new THREE.AmbientLight( 0x404040, 20 ); // soft white light
 scene.add( light );
-const directionalLight = new THREE.DirectionalLight( 0xff0000, 100000 );
+const directionalLight = new THREE.DirectionalLight( 0x404040, 100 );
 directionalLight.position.set(12.388, 3.760, -18);
 directionalLight.castShadow = true;
 scene.add( directionalLight );
@@ -63,13 +63,79 @@ loader.load(
 	}
 );
 
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+const sound = new THREE.Audio( listener );
+const sound2 = new THREE.Audio( listener );
+
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( './Audio/wind.mp3', function( buffer ) {
+  sound.setBuffer( buffer );
+  sound.setLoop( true );
+  sound.setVolume( 0.3 );
+  sound.play();
+});
+
+audioLoader.load( './Audio/04_cungdinhhue.mp3', function( buffer ) {
+    sound2.setBuffer( buffer );
+    sound2.setLoop( true );
+    sound2.setVolume( 0.2 );
+    sound2.play();
+});
+
+const sphere = new THREE.SphereGeometry( 1, 32, 32 );
+const texture = new THREE.TextureLoader().load( './Tex/football.jpg' );
+const material = new THREE.MeshPhongMaterial( { map: texture } );
+const ball = new THREE.Mesh( sphere, material );
+ball.position.set(2.626, 0.38, 3.084);
+ball.scale.set(0.05, 0.05, 0.05);
+ball.castShadow = true;
+scene.add( ball );
+
+const cs = new GLTFLoader();
+let mixer;
+cs.load(
+    './3D/03_canh_sat.glb',
+    function ( gltf ) {
+        const model = gltf.scene;
+        model.position.set(3.626, 0.31, 3.084);
+        model.scale.set(0.15, 0.15, 0.15);
+
+        const animation = gltf.animations;
+            if (animation && animation.length) {
+                mixer = new THREE.AnimationMixer(model);
+                animation.forEach((clip) => {
+                    mixer.clipAction(clip).play();
+                });
+            }
+        scene.add(model);
+    },
+    function ( xhr ) {
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    function ( error ) {
+        console.log( 'An error happened' );
+    }
+);
+
 function animate() {
     requestAnimationFrame(animate);
-
+    if (mixer) {
+      mixer.update(clock.getDelta())
+    };
     render();
   };
-  function render() {
+  
+const clock = new THREE.Clock();
+function render() {
     renderer.render(scene, camera);
   };
 
 animate();
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
